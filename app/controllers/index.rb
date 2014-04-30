ccd3c997a41f025cfccbaab7ffd23fd458a2433c
David Goodman <goodman.david.h@gmail.com>
2014-04-29 18:13:52 -0700

    deletes excess files and text

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,18 +1,3 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
-end
-
-post '/results' do
-	@name = Person.new(params[:name])
-	p @name
-	redirect '/results'
-
-	end
-
-get '/results' do
-	p @name
-		@name = params[:name]
-  
-  erb :gay
 end
