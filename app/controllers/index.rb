bbcd711fc887be8554e1384b399422721286b6e4
David Goodman <goodman.david.h@gmail.com>
2014-05-02 15:17:55 -0700

    wrking logic in neater format

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,11 +4,6 @@ get '/' do
 end
 
 post '/results' do
-	@words = Words.where("can_word = ?", Words.can(params[:letters]))
-	erb :results
-end
-
-post '/extra_results' do
-	@words = Words.excess(params[:letters])
+	@words = Search::find_bingos(params[:letters])
 	erb :results
 end
---
+++ app/helpers/search.rb
@@ -0,0 +1,29 @@
+module Search
+	def self.find_bingos(letters)
+		array_of_letters = letters.split('')
+		array_of_letters.include?("$") ? just_letters(array_of_letters) : blank_tile(array_of_letters)
+	end
+
+	def self.can(array_of_letters)
+		array_of_letters.sort.join
+	end
+
+	def self.just_letters(array_of_letters)
+		canned = array_of_letters.sort.join
+		Words.where("can_word = ?", canned)
+	end
+
+	def self.blank_tile(array_of_letters)
+		counter = 0
+		alph = ('a'..'z').to_a
+		results = []
+		26.times do
+			array_of_letters << alph[counter]
+			counter = counter + 1
+			canned = can(array_of_letters)
+			found_matches = Words.where("can_word = ?", canned)
+			found_matches.each {|match| results << match} if found_matches
+		end
+		results
+	end
+end
