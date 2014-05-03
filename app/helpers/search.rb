7d21d0bab6ade1b5c86217672861764eabc55bb1
David Goodman <goodman.david.h@gmail.com>
2014-05-02 18:42:32 -0700

    returns all daya

--- app/helpers/search.rb
+++ app/helpers/search.rb
@@ -1,7 +1,7 @@
 module Search
 	def self.find_bingos(letters)
 		array_of_letters = letters.split('')
-		array_of_letters.include?("$") ? just_letters(array_of_letters) : blank_tile(array_of_letters)
+		array_of_letters.include?("$") ? blank_tile(array_of_letters) : just_letters(array_of_letters)
 	end
 
 	def self.can(array_of_letters)
@@ -17,12 +17,14 @@ module Search
 		counter = 0
 		alph = ('a'..'z').to_a
 		results = []
+		array_of_letters.delete("$")
 		26.times do
 			array_of_letters << alph[counter]
 			counter = counter + 1
 			canned = can(array_of_letters)
 			found_matches = Words.where("can_word = ?", canned)
 			found_matches.each {|match| results << match} if found_matches
+			array_of_letters.pop
 		end
 		results
 	end
